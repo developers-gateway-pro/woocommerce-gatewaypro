b9fcfa308ac1fe7dd67f1793396e76e0dc181e7f
Carlos Arriero <desarrollo@dyner24.com>
2024-03-26 17:25:45 -0500

    first commit

---
+++ views/gateway-disabled.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Admin Panel Options.
+ */
+defined('ABSPATH') || exit;
+
+return
+'
+<div class="inline error">
+    <p>
+        <strong>'.esc_html_e('GatewayDisabled', 'GatewayPro').'</strong>:
+        '.esc_html_e('PagoSeguroNotCurrency.', 'GatewayPro').'
+    </p>
+</div>
+';
